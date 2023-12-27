985d1620a86d65a869f033daff06235208ec0ae9
Orlandini Sebastien <sebastien.orlandini@gmail.com>
2023-12-27 14:41:55 +0100

    27/12/2023

---
+++ 2103.RingsandRods.ts
@@ -0,0 +1,7 @@
+function countPoints(rings: string): number {
+    let rods = Array(10).fill("");
+    for(let i = 0; i < rings.length; i += 2){
+        if(!(rods[rings[i+1]].includes(rings[i]))) rods[rings[i+1]] += rings[i]
+    }
+    return rods.filter(rod => rod.length > 2).length
+};
